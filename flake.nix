794a7bde8b928cbf451adabd72264e60dd054195
Siva <39309626+InsaneDuck@users.noreply.github.com>
2024-11-15 18:05:43 +0000

    updated

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
     nixosConfigurations = {
         insaneduck-nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
             modules = [
             # Import the previous configuration.nix we used,
             # so the old configuration file still takes effect
